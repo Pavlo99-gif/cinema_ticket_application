22f85affc5fffad4c4f0587a4a28d38b0c53a803
Pavlo99-gif <kurulchyk7355608@gmail.com>
2022-08-01 11:42:34 +0300

    rename package

--- src/main/java/cinema_ticket/exception/DataProcessingException.java
+++
@@ -1,7 +0,0 @@
-package cinema_ticket.exception;
-
-public class DataProcessingException extends RuntimeException {
-    public DataProcessingException(String message, Throwable cause) {
-        super(message, cause);
-    }
-}
